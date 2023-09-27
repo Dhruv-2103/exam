8d31e593b08dc68af936b05dc21ef156860ee9ae
Dhruv Shah <103915592+Dhruv-2103@users.noreply.github.com>
2023-09-27 09:37:03 +0530

    Create Simple Graph.py
    Animal Speed Program

---
+++ Simple Graph.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+animal_names = ['Lion', 'Gazelle', 'Cheetah']
+mph_speed = [50, 60, 75]
+
+fig, ax = plt.subplots()
+bar_container = ax.bar(animal_names, mph_speed)
+ax.set(ylabel='speed in MPH', title='Running speeds', ylim=(0, 80))
